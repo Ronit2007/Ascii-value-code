8571670309ad45f5b8483c89c6d2faac0a741f86
Ronit2007 <72669781+Ronit2007@users.noreply.github.com>
2020-10-11 13:48:03 +0530

    This file is written in c language

---
+++ ASCII.c
@@ -0,0 +1,11 @@
+// A code to find ASKII value of any character.
+
+#include <stdio.h>
+int main()
+{
+    char chr;
+    printf("Enter a char you want ASCII value of \n");
+    scanf("%c",&chr);
+    printf("ASCII value of %c is %d \n",chr,chr);
+    return 0;
+}
